e24a1a994494ed29d1818e8a38ae6c10164f6752
A-EHJ <Abrahamelhage2003@gmail.com>
2024-07-15 17:39:44 -0400

    Agregando librerias

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,8 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("com.google.devtools.ksp") version "2.0.0-1.0.21"
+    alias(libs.plugins.kotlinx.serialization) apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
+    id("com.google.dagger.hilt.android") version "2.51" apply false
 }
