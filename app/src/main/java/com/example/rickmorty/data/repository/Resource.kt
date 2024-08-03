a89f8ec567eaf9c287dbe0083ef298bc1c323a50
A-EHJ <Abrahamelhage2003@gmail.com>
2024-08-03 18:16:28 -0400

    Cambiando de lugar

--- app/src/main/java/com/example/rickmorty/data/repository/Resource.kt
+++ app/src/main/java/com/example/rickmorty/data/repository/Resource.kt
@@ -1,4 +1,4 @@
-package com.example.rickmorty.data.repository.Net
+package com.example.rickmorty.data.repository
 
 sealed class Resource<T>(val data: T? = null, val message: String? = null) {
     class Loading<T>(data: T? = null) : Resource<T>(data)
