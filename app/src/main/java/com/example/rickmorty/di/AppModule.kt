c9a4b05109d92face2f1aa74762005296e805890
A-EHJ <Abrahamelhage2003@gmail.com>
2024-08-03 18:17:41 -0400

    Eliminando los provides innecesarios

--- app/src/main/java/com/example/rickmorty/di/AppModule.kt
+++ app/src/main/java/com/example/rickmorty/di/AppModule.kt
@@ -2,27 +2,17 @@ package edu.ucne.abrahamelhage_ap2_p1.di
 
 import android.content.Context
 import androidx.room.Room
-import com.example.rickmorty.data.Network.remote.Api.CharacterApi
-import com.example.rickmorty.data.Network.remote.Api.CharacterEpisodeApi
-import com.example.rickmorty.data.Network.remote.Api.EpisodeApi
-import com.example.rickmorty.data.Network.remote.Api.LocationApi
-import com.example.rickmorty.data.Network.remote.Api.LocationResidentApi
 import com.example.rickmorty.data.local.dao.CharacterDao
 import com.example.rickmorty.data.local.dao.CharacterEpisodeDao
 import com.example.rickmorty.data.local.dao.EpisodeDao
 import com.example.rickmorty.data.local.dao.LocationDao
 import com.example.rickmorty.data.local.dao.LocationResidentDao
 import com.example.rickmorty.data.local.database.RMDb
-import com.example.rickmorty.data.repository.Local.CharacterEpisodeLocalRepository
-import com.example.rickmorty.data.repository.Local.CharacterLocalRepository
-import com.example.rickmorty.data.repository.Local.EpisodeLocalRepository
-import com.example.rickmorty.data.repository.Local.LocationLocalRepository
-import com.example.rickmorty.data.repository.Local.LocationResidentLocalRepository
-import com.example.rickmorty.data.repository.Net.CharacterEpisodeNetRepository
-import com.example.rickmorty.data.repository.Net.CharacterNetRepository
-import com.example.rickmorty.data.repository.Net.EpisodeNetRepository
-import com.example.rickmorty.data.repository.Net.LocationNetRepository
-import com.example.rickmorty.data.repository.Net.LocationResidentNetRepository
+import com.example.rickmorty.data.network.remote.Api.CharacterApi
+import com.example.rickmorty.data.network.remote.Api.CharacterEpisodeApi
+import com.example.rickmorty.data.network.remote.Api.EpisodeApi
+import com.example.rickmorty.data.network.remote.Api.LocationApi
+import com.example.rickmorty.data.network.remote.Api.LocationResidentApi
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
@@ -77,76 +67,6 @@ object AppModule {
     @Provides
     fun provideLocationResidentDao(db: RMDb): LocationResidentDao {
         return db.locationResidentDao()
-    }
-
-    @Singleton
-    @Provides
-    fun provideCharacterLocalRepository(characterDao: CharacterDao): CharacterLocalRepository {
-        return CharacterLocalRepository(characterDao)
-    }
-
-    @Singleton
-    @Provides
-    fun provideEpisodeLocalRepository(episodeDao: EpisodeDao): EpisodeLocalRepository {
-        return EpisodeLocalRepository(episodeDao)
-    }
-
-    @Singleton
-    @Provides
-    fun provideLocationLocalRepository(locationDao: LocationDao): LocationLocalRepository {
-        return LocationLocalRepository(locationDao)
-    }
-
-    @Singleton
-    @Provides
-    fun provideCharacterEpisodeLocalRepository(characterEpisodeDao: CharacterEpisodeDao): CharacterEpisodeLocalRepository {
-        return CharacterEpisodeLocalRepository(characterEpisodeDao)
-    }
-
-    @Singleton
-    @Provides
-    fun provideLocationResidentLocalRepository(locationResidentDao: LocationResidentDao): LocationResidentLocalRepository {
-        return LocationResidentLocalRepository(locationResidentDao)
-    }
-
-    @Provides
-    @Singleton
-    fun provideCharacterNetRepository(
-        characterApi: CharacterApi
-    ): CharacterNetRepository {
-        return CharacterNetRepository(characterApi)
-    }
-
-    @Provides
-    @Singleton
-    fun provideCharacterEpisodeNetRepository(
-        characterEpisodeApi: CharacterEpisodeApi
-    ): CharacterEpisodeNetRepository {
-        return CharacterEpisodeNetRepository(characterEpisodeApi)
-    }
-
-    @Provides
-    @Singleton
-    fun provideLocationResidentNetRepository(
-        locationResidentApi: LocationResidentApi
-    ): LocationResidentNetRepository {
-        return LocationResidentNetRepository(locationResidentApi)
-    }
-
-    @Provides
-    @Singleton
-    fun provideLocaionNetRepository(
-        locationApi: LocationApi
-    ): LocationNetRepository {
-        return LocationNetRepository(locationApi)
-    }
-
-    @Provides
-    @Singleton
-    fun provideEpisodeNetRepository(
-        episodeApi: EpisodeApi
-    ): EpisodeNetRepository {
-        return EpisodeNetRepository(episodeApi)
     }
 
 
